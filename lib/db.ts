ff9ee98786ce0cb7712fa4b34d641876d0f95cb5
Luis Ariel Beltran <63753209+LuisArielBeltran@users.noreply.github.com>
2026-09-21 18:04:17 -0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,16 @@
-
-
 import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 3, // Limita las conexiones simultáneas para que Railway no sufra bloqueos
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
 });
 
 export default pool;
+
+
+
