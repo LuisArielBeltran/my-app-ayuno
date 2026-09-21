35e5904657b5ca898b493aab02b1fcf73190318c
Luis Ariel Beltran <63753209+LuisArielBeltran@users.noreply.github.com>
2026-09-21 16:09:39 -0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
-// lib/db.ts
 import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
