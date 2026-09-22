ef7416547a706beb8de2f76d17ad7e2e56e64f41
Luis Ariel Beltran <63753209+LuisArielBeltran@users.noreply.github.com>
2026-09-22 20:50:58 -0300

    Integrate SessionProviderWrapper in RootLayout
    Added SessionProviderWrapper to RootLayout for session management.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SessionProviderWrapper from '@/components/SessionProviderWrapper';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProviderWrapper>
+          {children}
+        </SessionProviderWrapper>
+      </body>
     </html>
   );
 }
